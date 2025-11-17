14f2be79f3a0e419d492631a241b6f73ec37f6b0
Zahid Hasan Milu <minions.milu889@gmail.com>
2025-11-17 16:39:56 +0600

    dumps, and parse data

--- documentation/consumers.py
+++ documentation/consumers.py
@@ -2,6 +2,8 @@ from channels.consumer import SyncConsumer, AsyncConsumer
 from channels.exceptions import StopConsumer
 from time import sleep
 import asyncio
+import json
+
 class MySyncConsumer(SyncConsumer):
     def websocket_connect(self, event):
         print("websocket connected...")
@@ -16,7 +18,7 @@ class MySyncConsumer(SyncConsumer):
         for i in range(5):
             self.send({
                 "type": "websocket.send",
-                "text": "Hello, World! " + str(i)
+                "text": json.dumps({"count":i}),
             })
             sleep(1)
         
@@ -39,7 +41,7 @@ class MyAsyncConsumer(AsyncConsumer):
         for i in range(5):
             await self.send({
                 "type": "websocket.send",
-                "text": "Hello, World! " + str(i)
+                "text": json.dumps({"count":i}),
             })
             await asyncio.sleep(1)        
     
